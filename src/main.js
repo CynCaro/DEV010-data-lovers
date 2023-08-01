383c528924aa80780bdf013ae476a13b68c127dc
CynCaro <cyn.caro.mtz@gmail.com>
2023-08-01 11:29:17 -0600

    Agrega estilos a los items y menú de navegación

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import data from './data/rickandmorty/rickandmorty.js';
 
 const container = document.querySelector(".flex-container");
 
-data.results.forEach((character) => {   //estamos recorriendo todos los personajes desde results del data
+data.results.forEach((character) => {   //estamos recorriendo todos los personajes desde results del data 
   const characterData = `
     <div class="flex-item">
       <img src="${character.image}">
@@ -11,12 +11,12 @@ data.results.forEach((character) => {   //estamos recorriendo todos los personaj
       <h3>${character.species}</h3>
       <h3>${character.origin.name}</h3>
       <h3>${character.location.name}</h3>
+      <button>Ver más</button> 
     </div>
   `;
 
   container.insertAdjacentHTML("beforeend", characterData); //con insertAdjacentHTML() agregamos al final del contenedor un elemento 
 
 });
-
 
 console.log(example, data);
